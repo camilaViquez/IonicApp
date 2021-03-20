d4196c1d8ddef57fe6380239c8d8f951d655c65b
camilaViquez <camila.viquez87@gmail.com>
2021-03-20 16:41:42 -0600

    Create reducers in reducers folder

---
+++ users-search/src/app/store/reducers/users.reducer.ts
@@ -0,0 +1,42 @@
+import { createReducer, on } from '@ngrx/store';
+import { User } from 'src/app/model/user.model';
+import { loadUsers } from 'src/app/store/actions';
+
+export interface UsersState {
+    users: User[],
+    loaded: boolean,
+    loading: boolean,
+    error: any
+
+}
+
+export const usersInitialState: UsersState = {
+    users: [],
+    loaded: false,
+    loading: false,
+    error: null
+
+}
+
+const _userReducer = createReducer(usersInitialState,
+     on(loadUsers, state => ({...state, loading: true})),
+     
+     on(loadUserSuccess), (state, { users }) => ({
+         ...state,
+         loading: false,
+         loaded: true,
+         users: [...users]
+
+
+     }),
+     on(loadUsersUnsuccess, (state, { payload }) => {
+         ...state,
+         loading: false,
+         loaded: false,
+         error: payload
+     })
+);
+
+export function userReducer(state, action) {
+    return _userReducer(state, action);
+}
